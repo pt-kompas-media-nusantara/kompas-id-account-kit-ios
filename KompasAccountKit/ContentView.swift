96e4a80543caedcf7c6b64bd24342244a0c3ccf6
nurirppan <nurirppan@gmail.com>
2025-08-15 19:59:24 +0700

    ! first push

---
+++ KompasAccountKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KompasAccountKit
+//
+//  Created by Kompas Digital on 15/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KompasAccountKit/KompasAccountKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KompasAccountKitApp.swift
+//  KompasAccountKit
+//
+//  Created by Kompas Digital on 15/08/25.
+//
+
+import SwiftUI
+
+@main
+struct KompasAccountKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
